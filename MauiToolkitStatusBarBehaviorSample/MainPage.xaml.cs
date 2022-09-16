2d507b8c7bad7f359b83ddf48a7f9240cc491d90
Gerald Versluis <gerald@verslu.is>
2022-09-16 20:53:53 +0200

    Implement StatusBarBehavior

--- MauiToolkitStatusBarBehaviorSample/MainPage.xaml.cs
+++ MauiToolkitStatusBarBehaviorSample/MainPage.xaml.cs
@@ -7,11 +7,20 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+		statusBar.StatusBarColor = Colors.Pink;
+        statusBar.StatusBarStyle = CommunityToolkit.Maui.Core.StatusBarStyle.DarkContent;
+
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
 	{
-		count++;
+        //statusBar.StatusBarColor = Colors.Red;
+        //statusBar.StatusBarStyle = CommunityToolkit.Maui.Core.StatusBarStyle.LightContent;
+
+		Navigation.PushAsync(new ContentPage());
+
+        count++;
 
 		if (count == 1)
 			CounterBtn.Text = $"Clicked {count} time";
--- MauiToolkitStatusBarBehaviorSample/MauiProgram.cs
+++ MauiToolkitStatusBarBehaviorSample/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiToolkitStatusBarBehaviorSample;
+﻿using CommunityToolkit.Maui;
+
+namespace MauiToolkitStatusBarBehaviorSample;
 
 public static class MauiProgram
 {
@@ -7,7 +9,8 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
